6ad0c79dd58db2aed6099487f90d2e8d9e0036bd
Haidezai <haidepzai269@gmail.com>
2026-04-16 11:28:11 +0700

    fix: exclude robots.txt and sitemap.xml from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -77,6 +77,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)'],
 };
 
